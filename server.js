bcdea96a6d53f512e9f7db5bfc69da6164ac08a8
Mohammad_Mohammad <mm6dd.mh@gmail.com>
2023-05-08 07:08:25 +0100

    first actual commit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=> {
     res.send('something to test')
 })
 
+app.get('/', (req, res)=> {
+    res.send('something to commit')
+})
+
 const PORT = 3000
 app.listen(PORT, (req, res)=> {
     console.log(`Server is listening on PORT ${PORT}`)
